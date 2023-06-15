d3bc93b0c626f53fd9e321f22b7fe428a5693f3c
utopusc <87100762+utopusc@users.noreply.github.com>
2023-06-15 22:34:33 +0000

    test

--- assets/mail/contact.php
+++ assets/mail/contact.php
@@ -42,7 +42,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "Info@yourdomain.com";
+$address = "burakcanoztruk@gmail.com";
 
 
 // Configuration option.
